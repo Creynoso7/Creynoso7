e13601ca4d20b1e8ce0e10361b8fe919f1689a5f
Creynoso7 <creynoso62103@gmail.com>
2024-10-12 21:20:54 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,32 +1,32 @@
-        $(document).ready(function() {
-            var answers = {
-                q1: "Arteries",
-                q2: "4"
-                q3: "4"
-                q4: "4"
-                q5: "4"
-            };
+    $(document).ready(function() {
+    var answers = {
+        q1: "Arteries",
+        q2: "4",
+        q3: "4",
+        q4: "4",
+        q5: "4"
+    };
 
-            $('#submitQuiz').click(function() {
-                var score = 0;
-                var totalQuestions = 5;
+    $('#submitQuiz').click(function() {
+        var score = 0;
+        var totalQuestions = 5;
 
-                if ($('input[name="q1"]:checked').val() === answers.q1) {
-                    score++;
-                }
-                if ($('input[name="q2"]:checked').val() === answers.q2) {
-                    score++;
-                }
-                if ($('input[name="q3"]:checked').val() === answers.q3) {
-                    score++;
-                }
-                if ($('input[name="q4"]:checked').val() === answers.q4) {
-                    score++;
-                }
-                if ($('input[name="q5"]:checked').val() === answers.q5) {
-                    score++;
-                }
-                    
-                $('#result').html("You scored " + score + " out of " + totalQuestions);
-            });
-        });
+        if ($('input[name="q1"]:checked').val() === answers.q1) {
+            score++;
+        }
+        if ($('input[name="q2"]:checked').val() === answers.q2) {
+            score++;
+        }
+        if ($('input[name="q3"]:checked').val() === answers.q3) {
+            score++;
+        }
+        if ($('input[name="q4"]:checked').val() === answers.q4) {
+            score++;
+        }
+        if ($('input[name="q5"]:checked').val() === answers.q5) {
+            score++;
+        }
+            
+        $('#result').html("You scored " + score + " out of " + totalQuestions);
+    });
+});
